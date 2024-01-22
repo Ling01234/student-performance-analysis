7ef54ad69cfbb7990b813b184ed9e32caba4c67e
Ling Fei Zhang <lzhang133@gmail.com>
2024-01-21 21:08:33 -0500

    logger and exception handling

---
+++ src/exception.py
@@ -0,0 +1,27 @@
+import logging
+import sys
+
+
+def error_message_details(error, detail: sys):
+    _, _, exc_tb = detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = f'Error occured in Python script name [{file_name}] line number [{exc_tb.tb_lineno}] error message [{str(error)}]'
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_details(error_message, error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+
+if __name__ == '__main__':
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info('Divide by Zero')
+        raise CustomException(e, sys)
